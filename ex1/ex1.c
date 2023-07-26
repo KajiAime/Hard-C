85d09a7c2091d6e6745f430163d32f2fb3fd8363
KajiAime <ndekaji@gmail.com>
2023-07-26 13:17:24 +0100

    I am currently doing exercise 10.

---
+++ ex1/ex1.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char *argv[]){
+	puts("Twinkle twinkle little star,");
+	puts("How I wonder what you are,");
+	puts("High above the sky so far,");
+	puts("Like a diamond in the sky,");
+	puts("When the blazing sun is gone,");
+	puts("When he nothing shines no more,");
+	puts("That is when you show your light,");
+	puts("Twinkle twinkle little star,");
+	return 0;
+}
---
+++ ex3/ex3.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(){
+	int age = 10;
+	float height = 72;
+
+	printf("I am %d	years old.\n",age);
+	printf("I am %.2f inches tall.\n",height);
+
+	return 0;
+}
+
